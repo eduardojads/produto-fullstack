090ec64fbb6474048c9b39a0a8ad7caa6122bc22
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-22 11:24:27 -0300

    theme, index.css and main

---
+++ frontend/src/theme/index.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme ({
+palette: {
+primary: { 
+    main: '#5f236ec1',
+},
+secondary: {
+    main:'#F0265D', // Exemplo de cor secundária
+},
+},
+});
