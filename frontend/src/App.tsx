16127b65b897a763dfad46c3473dadfc025eeeac
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-22 11:43:04 -0300

    add Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Box } from "@mui/material"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 function App() {
 
   return (
-    <Box>
+    <Box sx={ {display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
       <Header title="Gestão de Produtos"/>
+
+      <Footer/>
     </Box>
   )
 }
